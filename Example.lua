8acd406568da95d880694ccf73edf45c5e2f0c06
jamkless <jamkles@innocent.com>
2024-10-14 18:50:28 +0100

    Update Example.lua

--- Example.lua
+++ Example.lua
@@ -1,11 +1,11 @@
 -- New example script written by wally
 -- You can suggest changes with a pull request or something
 
-local repo = 'https://raw.githubusercontent.com/RobloxSkids/LinoriaMob/refs/heads/main/'
+local repo = 'https://raw.githubusercontent.com/jamkless/lineoria-mob/refs/heads/main/Library.lua'
 
 local Library = loadstring(game:HttpGet(repo .. 'Library.lua'))()
-local ThemeManager = loadstring(game:HttpGet(repo .. 'addons/ThemeManager.lua'))()
-local SaveManager = loadstring(game:HttpGet(repo .. 'addons/SaveManager.lua'))()
+local ThemeManager = loadstring(game:HttpGet(repo .. 'ThemeManager.lua'))()
+local SaveManager = loadstring(game:HttpGet(repo .. 'SaveManager.lua'))()
 
 local Window = Library:CreateWindow({
     -- Set Center to true if you want the menu to appear in the center
